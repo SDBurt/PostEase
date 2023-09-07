f91f3d9d93ad7b092d36dee47eb8556922338908
sdburt <seandburt@gmail.com>
2023-09-07 14:03:43 -0700

    chore(test): add test for isValidUrl and containsURL

---
+++ test/lib/urls.test.ts
@@ -0,0 +1,20 @@
+// sum.test.js
+import { containsURL, isValidUrl } from '@/lib/utils'
+import { expect, test } from 'vitest'
+
+
+test('isValidUrl on http://google.com returns true', () => {
+  expect(isValidUrl("https://google.com")).toEqual(true)
+})
+
+test('isValidUrl on google.com returns false', () => {
+  expect(isValidUrl("google.com")).toEqual(true)
+})
+
+test('containsURL on "google.com" returns "https://google.com"', () => {
+  expect(containsURL("google.com")).toEqual("https://google.com")
+})
+
+test('containsURL with input "the site postease.ca is cool" returns "https://postease.ca"', () => {
+  expect(containsURL("the site postease.ca is cool")).toEqual("https://postease.ca")
+})
